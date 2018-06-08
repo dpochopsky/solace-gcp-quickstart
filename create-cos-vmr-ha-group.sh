7b18701d7a39c44df32e530f15d620a576e32b2f
Christian <christian.accounts@gmx.de>
2018-06-08 13:31:03 +0100

    Add scripts for spinning up full HA group on GCE

---
+++ create-cos-vmr-ha-group.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+basename=cosvmr
+if [[ $# > 0 ]]; then
+for index in 0 1 2; do
+  name=${basename}${index}
+  if [ "${index}" == "0" ]; then
+    startupscript="gce_vmr_startup_monitor.sh"
+    bootdisksize=50GB
+    machinetype="n1-standard-2"
+    datadisk="${name}-datadisk"
+    datadisksize=10GB
+  elif [ "${index}" == "1" ]; then
+    startupscript="test.sh"
+    bootdisksize=200GB
+    machinetype="n1-standard-8"
+    datadisk="${name}-datadisk"
+    datadisksize=1000GB
+  else
+    startupscript="gce_vmr_startup_backup.sh"
+    bootdisksize=200GB
+    machinetype="n1-standard-8"
+    datadisk="${name}-datadisk"
+    datadisksize=1000GB
+  fi
+
+    gcloud compute disks create ${datadisk} \
+    --description="Data disk for VMR ${name}" \
+    --labels="creator=choltfurth,usage=perftesting,customer=hsbc" \
+    --size=${datadisksize} \
+    --type=pd-ssd \
+    --zone=europe-west1-b
+
+    gcloud compute instances create ${name} \
+    --boot-disk-size=${bootdisksize} \
+    --boot-disk-type=pd-standard \
+    --labels="creator=choltfurth,usage=cos-testing,customer=hsbc" \
+    --machine-type=${machinetype} \
+    --zone=europe-west1-b \
+    --disk="name=${datadisk},device-name=sdb,auto-delete=yes,mode=rw" \
+    --image-family=coreos-stable \
+    --image-project=coreos-cloud
+done
+sleep 60
+fi
+for index in 0 1 2; do
+  name=${basename}${index}
+  if [ "${index}" == "0" ]; then
+    startupscript="gce_vmr_startup_monitor.sh"
+  elif [ "${index}" == "1" ]; then
+    startupscript="gce_vmr_startup_primary.sh"
+  else
+    startupscript="gce_vmr_startup_backup.sh"
+  fi
+  gcloud compute scp ${startupscript} ${name}:~/install-vmr.sh
+  gcloud compute ssh ${name} --command="sudo ~/install-vmr.sh"
+done
---
+++ gce_vmr_startup_primary.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+##Select one of the three below to configure your monitor/primary/backup node
+echo "start-up script triggered..."
+#export vmr_role=monitor
+export vmr_role=primary
+#export vmr_role=backup
+##General section - edit as required
+export baseroutername=cosvmr
+export vmradminpass=soladmingce
+export vmr_scaling=10000 #1000, 10000 or 100000
+export monitor_ip=${baseroutername}0
+export primary_ip=${baseroutername}1
+export backup_ip=${baseroutername}2
+export redundancy_enable=yes
+export redundancy_group_password=mysolgrouppass
+##General section - no editing required
+export redundancy_group_node_${baseroutername}0_connectvia=${monitor_ip}
+export redundancy_group_node_${baseroutername}0_nodetype=monitoring
+export redundancy_group_node_${baseroutername}1_connectvia=${primary_ip}
+export redundancy_group_node_${baseroutername}1_nodetype=message_routing
+export redundancy_group_node_${baseroutername}2_connectvia=${backup_ip}
+export redundancy_group_node_${baseroutername}2_nodetype=message_routing
+if [ "${vmr_role}" == "monitor" ]; then
+##values for your monitor node
+  export nodetype=monitoring
+  export routername=${baseroutername}0
+elif [ "${vmr_role}" == "primary" ]; then
+##values for your primary node
+  export nodetype=message_routing
+  export routername=${baseroutername}1
+  export system_scaling_maxconnectioncount=${vmr_scaling}
+  export configsync_enable=yes
+  export redundancy_activestandbyrole=primary
+  export redundancy_matelink_connectvia=${backup_ip}
+elif [ "${vmr_role}" == "backup" ]; then
+##values for your backup node
+  export nodetype=message_routing
+  export routername=${baseroutername}2
+  export configsync_enable=yes
+  export redundancy_activestandbyrole=backup
+  export system_scaling_maxconnectioncount=${vmr_scaling}
+  export redundancy_matelink_connectvia=${primary_ip}
+else
+  echo "invalid role selected, disabling redundancy"
+  export redundancy_enable=no
+  export configsync_enable=no
+  unset redundancy_group_node_${baseroutername}0_connectvia
+  unset redundancy_group_node_${baseroutername}0_nodetype
+  unset redundancy_group_node_${baseroutername}1_connectvia
+  unset redundancy_group_node_${baseroutername}1_nodetype
+  unset redundancy_group_node_${baseroutername}2_connectvia
+  unset redundancy_group_node_${baseroutername}2_nodetype
+fi
+###
+if [ ! -d /var/lib/solace ]; then
+  echo "Done, starting install..."
+  echo "creating directory..."
+  mkdir /var/lib/solace
+  cd /var/lib/solace
+  yum install -y wget
+  LOOP_COUNT=0
+  echo "downloading VMR install script..."
+  while [ $LOOP_COUNT -lt 3 ]; do
+    wget https://raw.githubusercontent.com/ChristianHoltfurth/solace-gcp-quickstart/container-optimized-os/vmr-install.sh
+    if [ 0 != `echo $?` ]; then
+      ((LOOP_COUNT++))
+    else
+      break
+    fi
+  done
+  if [ ${LOOP_COUNT} == 3 ]; then
+    echo "`date` ERROR: Failed to download initial install script, exiting"
+    exit 1
+  fi
+  chmod +x /var/lib/solace/vmr-install.sh
+  bash /var/lib/solace/vmr-install.sh -i https://products.solace.com/download/PUBSUB_DOCKER_STAND -p ${vmradminpass}
+fi
