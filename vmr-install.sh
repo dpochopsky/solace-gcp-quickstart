f1baf8534bb61d784b009628b51036cd842524d0
Christian <christian.accounts@gmx.de>
2018-06-12 20:55:32 +0100

    fixed docker create

--- vmr-install.sh
+++ vmr-install.sh
@@ -140,8 +140,9 @@ fi
 
 echo "`date` INFO:Create a Docker instance from Solace Docker image" | tee -a ${LOG_FILE}
 # -------------------------------------------------------------
-VMR_VERSION=`docker images | grep solace | awk '{print $2}'`
 VMR_TYPE=`docker images | grep solace | awk '{print $1}'`
+VMR_VERSION=`docker images | grep solace | awk '{print $2}'`
+echo "VMR image retrieved is: ${VMR_TYPE}"
 echo "VMR version retrieved is: ${VMR_VERSION}"
 
 SOLACE_CLOUD_INIT="--env SERVICE_SSH_PORT=2222"
@@ -180,7 +181,7 @@ docker create \
    -v adbBackup:/usr/sw/adb \
    -v softAdb:/usr/sw/internalSpool/softAdb \
    ${SOLACE_CLOUD_INIT} \
-   --name=${VMR_TYPE}:${VMR_VERSION} | tee -a ${LOG_FILE}
+   --name=solace ${VMR_TYPE}:${VMR_VERSION} | tee -a ${LOG_FILE}
 
 #
 docker ps -a | tee -a ${LOG_FILE}
