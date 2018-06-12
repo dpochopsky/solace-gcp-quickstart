076be550b5b5bd3960552264b8114c1e98f479db
ChristianHoltfurth <christian.holtfurth@maplequad.com>
2018-06-12 14:27:06 +0100

    updating scripts for singleton

---
+++ create-centos-vmr-singleton.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+basename=centosvmr
+name=${basename}
+startupscript="gce_vmr_startup_singleton.sh"
+bootdisksize=500GB
+machinetype="n1-standard-8"
+datadisk="${name}-datadisk"
+datadisksize=1000GB
+
+gcloud compute disks create ${datadisk} \
+--description="Data disk for VMR ${name}" \
+--labels="usage=solace-vmr-centos" \
+--size=${datadisksize} \
+--type=pd-ssd \
+--zone=europe-west1-b
+
+gcloud compute instances create ${name} \
+--boot-disk-size=${bootdisksize} \
+--boot-disk-type=pd-standard \
+--labels="usage=solace-vmr-centos" \
+--machine-type=${machinetype} \
+--zone=europe-west1-b \
+--disk="name=${datadisk},device-name=sdb,auto-delete=yes,mode=rw" \
+--image-project=centos-cloud \
+--image-family=centos-7
+
+sleep 60
+
+name=${basename}
+startupscript="gce_vmr_startup_singleton.sh"
+gcloud compute scp ${startupscript} ${name}:~/install-vmr.sh
+gcloud compute ssh ${name} --command="sudo ~/install-vmr.sh"
