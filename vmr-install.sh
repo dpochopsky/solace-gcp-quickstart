10b97248bb2c3ac61c17fbddfe87f5fb08d261e5
Christian <christian.accounts@gmx.de>
2018-06-12 19:51:39 +0100

    make load work both for standard and eval version

--- vmr-install.sh
+++ vmr-install.sh
@@ -163,6 +163,7 @@ fi
 echo "`date` INFO:Create a Docker instance from Solace Docker image" | tee -a ${LOG_FILE}
 # -------------------------------------------------------------
 VMR_VERSION=`docker images | grep solace | awk '{print $2}'`
+VMR_TYPE=`docker images | grep solace | awk '{print $1}'`
 echo "VMR version retrieved is: ${VMR_VERSION}"
 
 SOLACE_CLOUD_INIT="--env SERVICE_SSH_PORT=2222"
@@ -201,7 +202,7 @@ docker create \
    -v adbBackup:/usr/sw/adb \
    -v softAdb:/usr/sw/internalSpool/softAdb \
    ${SOLACE_CLOUD_INIT} \
-   --name=solace solace-pubsub-standard:${VMR_VERSION} | tee -a ${LOG_FILE}
+   --name=${ VMR_TYPE}:${VMR_VERSION} | tee -a ${LOG_FILE}
 
 #
 docker ps -a | tee -a ${LOG_FILE}
