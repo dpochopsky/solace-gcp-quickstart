997ae869e082dbf8d045c90a52d3125d6b8c24c1
dpochopsky <david.pochopsky@solacesystems.com>
2018-07-27 18:22:55 -0400

    Add files via upload

--- vmr-install.sh
+++ vmr-install.sh
@@ -231,7 +231,7 @@ tee /usr/local/sbin/solace-container-exec-start-pre <<-EOF
 # Must port forward all packets with dest IP of the load balancer to the internal IP of VMR
 lbIP=`ip route list table local | grep "proto 66" | awk '{print $2}'`
 vmrIP=`ifconfig eth0 | grep "inet " | awk '{print $2}'`
-iptables -t nat -A PREROUTING -d \$lbIP -j DNAT --to-destination \$vmrIP
+#iptables -t nat -A PREROUTING -d \$lbIP -j DNAT --to-destination \$vmrIP
 
 exit 0
 EOF
